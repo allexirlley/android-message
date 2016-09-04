70258db12f1d169b22fbfc666515ea675fbd09d5
Thiago Porto <tporto88@gmail.com>
2016-09-03 23:51:44 -0300

    Add toolbar

--- app/src/main/java/tporto/com/message/LoginActivity.java
+++ app/src/main/java/tporto/com/message/LoginActivity.java
@@ -4,14 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 import tporto.com.message.application.ConfiguracaoFirebase;
+import tporto.com.message.model.Usuario;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Firebase firebase;
+    private EditText e_email, e_senha;
+    private Button b_logar;
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +28,55 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         firebase = ConfiguracaoFirebase.getFirebase();
+        verificarUsuarioLogado();
+
+        e_email = (EditText) findViewById(R.id.e_email);
+        e_senha = (EditText) findViewById(R.id.e_senha);
+        b_logar = (Button) findViewById(R.id.b_logar);
+
+        b_logar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                usuario = new Usuario();
+                usuario.setEmail(e_email.getText().toString());
+                usuario.setSenha(e_senha.getText().toString());
+                validarLogin();
+            }
+        });
     }
 
     public void abrirCadastro(View v){
         Intent intent = new Intent(LoginActivity.this,CadastroUsuarioActivity.class);
         startActivity(intent);
+    }
+
+    private void validarLogin(){
+        firebase.authWithPassword(
+                usuario.getEmail(),
+                usuario.getSenha(),
+                new Firebase.AuthResultHandler() {
+                    @Override
+                    public void onAuthenticated(AuthData authData) {
+                        abrirTelaPrincipal();
+                    }
+
+                    @Override
+                    public void onAuthenticationError(FirebaseError firebaseError) {
+                        Toast.makeText(LoginActivity.this,firebaseError.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+    }
+
+    private void abrirTelaPrincipal(){
+        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void verificarUsuarioLogado(){
+        if(firebase.getAuth() != null){
+            abrirTelaPrincipal();
+        }
     }
 }
--- app/src/main/java/tporto/com/message/MainActivity.java
+++ app/src/main/java/tporto/com/message/MainActivity.java
@@ -1,13 +1,56 @@
 package tporto.com.message;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.firebase.client.Firebase;
+
+import tporto.com.message.application.ConfiguracaoFirebase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Firebase firebase;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebase = ConfiguracaoFirebase.getFirebase();
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Message");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item_sair:
+                logout();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void logout(){
+        firebase.unauth();
+        Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
